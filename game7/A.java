6a45464f0e09d4b9aa765ee2c053906d26941eb6
mountainmomoi <18174@hakodate.kosen-ac.jp>
2022-11-24 10:27:37 +0900

    Aが移動処理を追加

--- game7/A.java
+++ game7/A.java
@@ -14,6 +14,9 @@ public class A extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(-90);
+            move(1);
+        }
     }    
 }
