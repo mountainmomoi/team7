0c233b9abda14bd5fe741bf238df4439e37cedc4
mountainmomoi <18174@hakodate.kosen-ac.jp>
2022-12-08 10:22:06 +0900

    移動追加

--- game7/A.java
+++ game7/A.java
@@ -12,8 +12,12 @@ public class A extends Actor
      * Act - do whatever the A wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
     public void act() 
     {
-        // Add your action code here.
-    }    
+        if(Greenfoot.isKeyDown("a")) setLocation(getX()+3, getY());
+        if(Greenfoot.isKeyDown("d")) setLocation(getX()-3, getY());
+        if(Greenfoot.isKeyDown("s")) setLocation(getX(), getY()+3);
+        if(Greenfoot.isKeyDown("w")) setLocation(getX(), getY()-3);
+    }
 }
