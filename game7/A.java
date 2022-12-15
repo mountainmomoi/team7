26d66a46c604a0a50d2fff732435da2b9d7631d8
YSD41 <18177@hakodate.kosen-ac.jp>
2022-12-15 10:28:30 +0900

    タイトル追加

--- game7/A.java
+++ game7/A.java
@@ -16,8 +16,8 @@ public class A extends Actor
     public void act() 
     {
 
-        if(Greenfoot.isKeyDown("a")) setLocation(getX()+3, getY());
-        if(Greenfoot.isKeyDown("d")) setLocation(getX()-3, getY());
+        if(Greenfoot.isKeyDown("d")) setLocation(getX()+3, getY());
+        if(Greenfoot.isKeyDown("a")) setLocation(getX()-3, getY());
         if(Greenfoot.isKeyDown("s")) setLocation(getX(), getY()+3);
         if(Greenfoot.isKeyDown("w")) setLocation(getX(), getY()-3);
     }
