9d8ce845765038b5f368a060bbe8af0e43019810
hasemyu0726 <mu3piyohiko@gmail.com>
2022-12-22 09:20:20 +0900

    豚ランダム移動追加

--- game7/C.java
+++ game7/C.java
@@ -1,19 +1,44 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
 /**
  * Write a description of class C here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
+
 public class C extends Actor
 {
     /**
      * Act - do whatever the C wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+
     public void act() 
     {
-        // Add your action code here.
-    }    
+        Actor actor = getOneIntersectingObject(A.class);
+        if(actor != null)
+        {
+            Greenfoot.stop();
+        }
+
+        switch( (int)(Math.random() * 4))
+        {
+            case 0:
+                setRotation(0);
+                move(40);
+                break;
+            case 1:
+                setRotation(90);
+                move(40);
+                break;
+            case 2:
+                setRotation(180);
+                move(40);
+                break;
+            case 3:
+                setRotation(270);
+                move(40);
+                break;
+        }
+    }
 }
